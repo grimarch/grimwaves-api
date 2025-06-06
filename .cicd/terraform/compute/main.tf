bbdefd96c28eb9cc6b1d2c6295e50598bd191428
grimarch <denis@zwinger.pro>
2025-06-06 09:42:21 +0200

    feat: add Terraform configuration for DigitalOcean provider with version constraints

--- .cicd/terraform/compute/main.tf
+++ .cicd/terraform/compute/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.34"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
 provider "digitalocean" {
   token = var.do_token
 }
